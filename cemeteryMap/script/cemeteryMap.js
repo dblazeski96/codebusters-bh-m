14d55b9384149950030cd21b89a88fe67d21bb7b
d3jan <dblazeski96@gmail.com>
2018-02-27 16:45:58 +0100

    Small animation update

--- cemeteryMap/script/cemeteryMap.js
+++ cemeteryMap/script/cemeteryMap.js
@@ -1,11 +1,12 @@
 $(document).ready(() => {
     let allItems = $("#mapImg, #gotoGate, #gotoArtifacts, #gotoStories");
+    let fadeDuration = 1000;
 
     allItems.hide();
-    allItems.show("fade", 1000);
+    allItems.show("fade", fadeDuration);
     
     $("#gotoGate").on("click", () => {
-        allItems.effect("fade", 1000, () => {
+        allItems.effect("fade", fadeDuration, () => {
             document.title = "Beit Haim";
             $("nav").hide();
             $("body").css("background-color", "black");
--- homepage/script/homepage.js
+++ homepage/script/homepage.js
@@ -1,19 +1,21 @@
 $(document).ready(() => {
     let allItemsHomepage = $("#gateImg, #gateEnter, #gateInfo");
+    let fadeDuration = 1000;
 
     allItemsHomepage.hide();
-    $("#gateImg").show("fade", 1000, () => {
-        $("#gateEnter, #gateInfo").show("fade", 1000);
+    $("#gateImg").show("fade", fadeDuration, () => {
+        $("#gateEnter, #gateInfo").show("fade", fadeDuration);
     });
     
     allItemsHomepage.on("click", () => {
-        $("#gateEnter, #gateInfo").hide("fade", 1000, () => {
-            $("#gateImg").removeClass("img-fluid").effect("scale", {scale: "both", percent: 400}, 2500, () => {
-                document.title = "Beit Haim | Cemetery Map";
-                $("body").css("background-color", "#242424")
-                $("nav").show();
-                $("#mainDiv").empty()
-                    .load("cemeteryMap/cemeteryMap.html");
+        $("#gateEnter, #gateInfo").hide("fade", fadeDuration, () => {
+            $("#gateImg").removeClass("img-fluid")
+                .effect("puff", {percent: 400}, 2500, () => {
+                    document.title = "Beit Haim | Cemetery Map";
+                    $("body").css("background-color", "#242424")
+                    $("nav").show();
+                    $("#mainDiv").empty()
+                        .load("cemeteryMap/cemeteryMap.html");
             });
         });
     });
