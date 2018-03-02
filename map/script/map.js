adbd2f6f49eac8f685a0397be114d9342b886496
d3jan <dblazeski96@gmail.com>
2018-03-02 18:02:25 +0100

    Map update and small homepage fix

--- map/script/map.js
+++ map/script/map.js
@@ -1,22 +1,44 @@
 let allItemsCM = $("#mapImg, #gotoGate, #gotoArtifacts, #gotoStories");
 let animsDuration = 800;
+let imgs = 2;
 
 $(document).ready(() => {
     $("body").css("background-image", 'url("../common/img/bg.png")');
 
     $(window).on("resize", () => {
         updateGotos(false);
+        updatePopup();
     });
 
     $("nav, nav *").show("blind", {direction: "up"}, animsDuration);
     $("#mapImg").show("fade", animsDuration);
     updateGotos(true);
 
+    $("#popupImg").on("click", () => {
+        $("#popupDiv, #dimDiv").hide("fade", animsDuration);
+    });
+
     $("#gotoGate").on("click", () => {
-        allItemsCM.hide("fade", animsDuration, () => {
+        $("#mainDiv").hide("fade", animsDuration, () => {
             document.location.href = "../homepage/homepage.html";
         });
+    }).on("mouseover", () => {
+        $("#gotoGate").children("img").attr("src", `img/gotoHover.png`).css("cursor", "pointer");
+    }).on("mouseout", () => {
+        $("#gotoGate").children("img").attr("src", `img/goto.png`);
     });
+
+    for(let n = 1; n <= imgs; n += 1){
+        $(`#gotoImg${n}`).on("click", () => {
+            $("#popupImg").attr("src", `img/popupImg/${n}.jpg`);
+            updatePopup();
+            $("#popupDiv, #dimDiv").show("fade", animsDuration);
+        }).on("mouseover", () => {
+            $(`#gotoImg${n}`).children("img").attr("src", `img/gotoHover.png`).css("cursor", "pointer");
+        }).on("mouseout", () => {
+            $(`#gotoImg${n}`).children("img").attr("src", `img/goto.png`);
+        });
+    }
 });
 
 function updateGotos(doAnims){
@@ -24,17 +46,23 @@ function updateGotos(doAnims){
         "margin-top": $("#mainDiv").height() * 0.8,
         "margin-left": $("#mainDiv").width() * 0.5
     });
-    $("#gotoStories").css({
+    $("#gotoImg1").css({
         "margin-top": $("#mainDiv").height() * 0.4,
         "margin-left": $("#mainDiv").width() * -0.1
     });
-    $("#gotoArtifacts").css({
+    $("#gotoImg2").css({
         "margin-top": $("#mainDiv").height() * 0.6,
         "margin-left": $("#mainDiv").width() * -0.5
     });
     if(doAnims){
         $("#gotoGate").show("fade", animsDuration);
-        $("#gotoStories").show("fade", animsDuration);
-        $("#gotoArtifacts").show("fade", animsDuration);
+        for(let n = 1; n <= imgs; n += 1){
+            $(`#gotoImg${n}`).show("fade", animsDuration);
+        }
     }
+}
+
+function updatePopup(){
+    $("#popupDiv").css("margin-top", window.innerHeight * 0.1);
+    $("#popupImg").css("height", window.innerHeight * 0.7);
 }
