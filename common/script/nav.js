d652d0cf2945f45045c15ec0ba297769dee80f1a
d3jan <dblazeski96@gmail.com>
2018-03-10 01:59:24 +0100

    Whole new update

--- common/script/nav.js
+++ common/script/nav.js
@@ -1,47 +1,51 @@
-$("body").prepend(`
-<nav class="navbar navbar-custom">
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link nav-link-custom" id="navGate" href="#">BEIT HAIM</a>
-        </li>
-    </ul>
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link nav-link-custom" id="navStories" href="#">STORIES</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link nav-link-custom" id="navMap" href="#">CEMETERY MAP</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link nav-link-custom" id="navShop" href="#">SHOP</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link nav-link-custom" id="navAbout" href="#">ABOUT THE PROJECT</a>
-        </li>
-    </ul>
-</nav>
-`);
+let navAnimsDuration = 800;
 
-$("#navGate").on("click", () => {
-    $("nav, nav *").hide("blind", {direction: "up"}, animsDuration, () => {
-        document.location.href = "../homepage/homepage.html";
+$(document).ready(() => {
+    $("body").prepend(`
+    <nav class="navbar navbar-custom">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" id="navGate" href="#">BEIT HAIM</a>
+            </li>
+        </ul>
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" id="navStories" href="#">STORIES</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" id="navTimeline" href="#">TIMELINE</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" id="navForum" href="#">FORUM</a>
+            </li>
+        </ul>
+    </nav>
+    `);
+
+    $("#navGate").on("click", () => {
+        $("nav, nav *").hide("blind", {direction: "up"}, navAnimsDuration, () => {
+            document.location.href = "../homepage/homepage.html";
+        });
+    });
+    
+    $("#navStories").on("click", () => {
+        $("nav, nav *").hide("blind", {direction: "up"}, navAnimsDuration, () => {
+            document.location.href = "../slider/slider.html";
+        });
+    });
+    
+    $("#navTimeline").on("click", () => {
+        $("nav, nav *").hide("blind", {direction: "up"}, navAnimsDuration, () => {
+            document.location.href = "../timeline/timeline.html";
+        });
+    });
+    
+    $("#navForum").on("click", () => {
+        $("nav, nav *").hide("blind", {direction: "up"}, navAnimsDuration, () => {
+            document.location.href = "../forum/forum.html";
+        });
     });
 });
 
-$("#navMap").on("click", () => {
-    $("nav, nav *").hide("blind", {direction: "up"}, animsDuration, () => {
-        document.location.href = "../map/map.html";
-    });
-});
 
-$("#navStories").on("click", () => {
-    $("nav, nav *").hide("blind", {direction: "up"}, animsDuration, () => {
-        document.location.href = "../slider/slider.html";
-    });
-});
 
-$("#navShop").on("click", () => {
-    $("nav, nav *").hide("blind", {direction: "up"}, animsDuration, () => {
-        document.location.href = "../shop/shop.html";
-    });
-});
